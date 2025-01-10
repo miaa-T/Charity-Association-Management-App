36e021902ccff3c3d0f03da8075a0398dbaec577
Mahdia-toubal <mahdia.toubal@gmail.com>
2025-01-10 12:44:19 +0100

    login works

--- views/Membre/navbar.php
+++ views/Membre/navbar.php
@@ -1,15 +1,16 @@
 <?php
+session_start();
 $current_page = basename($_SERVER['PHP_SELF']);
 ?>
 <header class="navbar">
-<link rel="stylesheet" href="styles.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <div class="logo">
         <a href="index.php"><img src="../Images/logo_elmountada.png" alt="Logo"></a>
     </div>
     <nav>
         <ul class="nav-links">
-              <li><a href="index.php" class="<?= $current_page == 'index.php' ? 'active' : '' ?>">Acceuil</a></li>
+            <li><a href="index.php" class="<?= $current_page == 'index.php' ? 'active' : '' ?>">Acceuil</a></li>
             <li><a href="Actualites.php" class="<?= $current_page == 'Actualites.php' ? 'active' : '' ?>">Actualités</a></li>
             <li><a href="partenaires.php" class="<?= $current_page == 'partenaires.php' ? 'active' : '' ?>">Partenaires</a></li>
             <li><a href="remises.php" class="<?= $current_page == 'remises.php' ? 'active' : '' ?>">Remises et Avantages</a></li>
@@ -23,8 +24,12 @@ $current_page = basename($_SERVER['PHP_SELF']);
         <a href="#"><i class="fa fa-instagram"></i></a>
     </div>
     <div class="profile-pic">
-        <a href="monprofile.php">
-            <img src="../Images/userPic.jpg" alt="Profile Picture">
-        </a>
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <a href="monprofile.php">
+                <img src="<?= $_SESSION['photo'] ?>" alt="Profile Picture">
+            </a>
+        <?php else: ?>
+            <a href="login.php">Connexion</a>
+        <?php endif; ?>
     </div>
 </header>
